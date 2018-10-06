13b9290af91f3c5fa3e8539a226a9b052ba527f2
elimerins <elimerins@gmail.com>
2018-10-06 16:05:26 +0900

    modifiy urls.py

--- kakaobotapp/urls.py
+++ kakaobotapp/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import url
+from django.urls import path,include
 
 from .import views
 
 urlpatterns=[
-    path(,views.keyboard),
-    path(",views.index),
-    path(",views
+    path('keyboard/',views.keyboard),
+    path('',views.index),
+    path('message',views.answer)
+]
--- kakaobotdjanogo/urls.py
+++ kakaobotdjanogo/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include,path
 
 urlpatterns = [
-    #path(",include('kakaobotapp.urls')),
+    path('',include('kakaobotapp.urls')),
     path('admin/', admin.site.urls),
 ]
